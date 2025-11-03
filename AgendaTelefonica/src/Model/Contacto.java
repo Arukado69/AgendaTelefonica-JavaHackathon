53cfdd9ab57f31a843ef6c91773975b6994c40e5
zepahua25 <jdaniel.zepahua@gmail.com>
2025-11-03 13:51:27 -0600

    3-crear-clase-agendajava
    Añadir logica de agenda

--- AgendaTelefonica/src/Model/Contacto.java
+++ AgendaTelefonica/src/Model/Contacto.java
@@ -2,13 +2,14 @@ package Model;
 
 public class Contacto {
 
-    private int id;
+    private int nextId = 1;
+    public int id;
     private String nombre;
     private String apellido;
     private String telefono;
 
-    public Contacto (int id, String nombre, String apellido, String telefono){
-        this.id = id;
+    public Contacto (String nombre, String apellido, String telefono){
+        this.id = nextId++;
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
